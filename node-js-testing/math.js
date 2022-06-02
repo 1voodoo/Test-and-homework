354b49221a5b8ffe3fa4f4846930be7d91c7f8b9
IHAR <pixel0101@mail.ru>
2022-06-02 14:11:09 +0300

    lesson 01

---
+++ node-js-testing/math.js
@@ -0,0 +1,13 @@
+function square(num) {
+    return num * num
+};
+function cub(num) {
+    return num * num * num
+};
+function cub4(num) {
+    return num * num * num * num
+};
+
+exports.square = square;
+exports.cub = cub;
+exports.cub4 = cub4;
--- node-js-testing/test.js
+++ node-js-testing/test.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+let fs = require('fs');
+let sameText = 'Hello World'
+let arr = ['once', 'exits', 'urqial']
+// let count = 1;
+// setInterval(() => {
+//     console.log(count++);
+// },1000);
+
+// let math = require('./math.js')
+// console.log(math.cub4(10));
+
+
+fs.readFile('read.txt', 'utf-8', (error, data) => {
+    if(error) console.log(error); 
+    let result = data.split(',');
+    let res = 0;
+    for (const num of result) {
+        res = res + Number(num);
+    };
+    let midiamNumber = res / result.length
+    fs.writeFileSync('read.txt', midiamNumber.toString());
+    fs.writeFileSync('newRead.txt', sameText + ' ' + 'what');
+    for (let i = 0; i < arr.length; i++) {
+        fs.writeFileSync(arr[i] + '.txt', '');
+    }  
+});
+
+fs.rename('test.txt', 'folder/test.txt', (error) => {
+    if(error) console.log(error);
+    console.log('add file');
+});
+ 
+
+
